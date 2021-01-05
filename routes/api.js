fd6b34dca174f1742b9a218ab28b93d6b88e89d6
RoronuaHong <807672412@qq.com>
2021-01-06 00:34:58 +0800

    deploy

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+  
+const express = require('express');
+
+const router = express.Router();
+
+// Routes
+router.get('/', (req, res) => {
+
+});
+
+router.post('/save', (req, res) => {
+    
+});
+
+
+router.get('/name', (req, res) => {
+   
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const path = require("path");
-const PORT = 8000;
 
-app.use(express.static(path.join(__dirname, 'slimhongweb', 'build')));
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const routes = require('./routes/api');
+
+mongoose.connect(process.env.MONGOOB_URI || 'mongodb+srv://slimhong:a1234560@cluster0.tp4ak.mongodb.net/users?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true 
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!');
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('Error!', error);
+});
+
+// Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// HTTP request logger
+app.use(morgan('tiny'));
+app.use('/api', routes);
+
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, 'slimhongweb', 'build')));
+}
 
 app.listen(PORT, ()=> {
     console.log(`Server Listening on port ${PORT}`);
 });
-
