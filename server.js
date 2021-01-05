bf85adff540221fca7c41503a6bac8c87009fa79
RoronuaHong <807672412@qq.com>
2021-01-06 01:11:47 +0800

    deploy

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 app.use('/api', routes);
 
-if(process.env.NODE_ENV == 'production') {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 
