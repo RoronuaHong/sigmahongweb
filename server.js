ec4b3458ce61fdbb1ed04bfd81016a4185469a80
RoronuaHong <807672412@qq.com>
2021-01-06 01:27:36 +0800

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const routes = require('./routes/api');
 
