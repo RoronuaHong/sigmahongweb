29a371d63175b57413ec010dfeec75da1241b742
RoronuaHong <807672412@qq.com>
2021-01-05 23:43:08 +0800

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const PORT = 8000;
 
-app.use(express.static(path.join(__dirname, 'hongweb', 'build')));
+app.use(express.static(path.join(__dirname, 'slimhongweb', 'build')));
 
 app.listen(PORT, ()=> {
     console.log(`Server Listening on port ${PORT}`);
