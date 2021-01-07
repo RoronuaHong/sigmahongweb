2d397680f3b6b8867e6dcd005da1419c767f79cb
RoronuaHong <807672412@qq.com>
2021-01-07 12:50:40 +0800

    google analysis

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,10 @@ import './Styles/main.css';
 import './Styles.css';
 import 'antd/dist/antd.css';
 
-import ReactGa from 'react-ga';
 import HomeApp from './Containers/HomePageContainer';
-import { useEffect, React } from 'react';
-
-ReactGa.initialize('G-R5JQHPKSLG');
+import { React } from 'react';
 
 function App() {
-  useEffect(() => {
-    // to report page view
-    ReactGa.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <div className='App'>
         <HomeApp />
