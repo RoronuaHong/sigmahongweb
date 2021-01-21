27be80a734624276fca8fa20f04e34f1fc520dfa
RoronuaHong <807672412@qq.com>
2021-01-21 20:59:46 +0800

    add images part

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 }
 
+app.use(`/images`, express.static('images'))
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
