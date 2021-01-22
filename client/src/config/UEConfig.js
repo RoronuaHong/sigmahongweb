46addd30cebe3894fdd5454322cce0c7a3461406
RoronuaHong <807672412@qq.com>
2021-01-22 22:49:49 +0800

    add ue homepage part

--- client/src/config/UEConfig.js
+++ client/src/config/UEConfig.js
@@ -1,13 +1,25 @@
+import UEBasis from '../assets/Images/HomePage/ue-basis.png'
 import UEInside from '../assets/Images/HomePage/ue-inside.png'
+import UEProd from '../assets/Images/HomePage/ue-prod.png'
 import UEGraphics from '../assets/Images/HomePage/ue-graphics.png'
 
 const UEList = [{
   id: 0,
+  name: `Basis`,
+  link: `/ue-basis`,
+  img: UEBasis
+}, {
+  id: 1,
   name: `Inside`,
   link: `/ue-inside`,
   img: UEInside
 }, {
-  id: 1,
+  id: 2,
+  name: `Production`,
+  link: `/ue-production`,
+  img: UEProd
+}, {
+  id: 3,
   name: `Graphics`,
   link: `/ue-graphics`,
   img: UEGraphics
